e56e1c9d2bfb968f27912bb1f9156ede4d414ef8
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-08-25 21:57:59 +0300

    playing with variables

---
+++ lab1/src/variables.ts
@@ -0,0 +1,12 @@
+let s: string = "hello";
+let n: number = 5;
+let f: boolean = true;
+let v: any = {"name": "Vasya"};
+
+console.log(v, n, f, v);
+
+let arr1: number[] = [1, 2, 3];
+let arr2: string[] = ["a", "b", "c"];
+
+console.log(arr1, arr2);
+
