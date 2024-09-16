de95807b5ea031e62837a61ce1bfc104c626d3e9
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-09-16 19:49:46 +0300

    implemented LibraryService class

--- lab3/src/services.ts
+++ lab3/src/services.ts
@@ -1,6 +1,33 @@
-import {Book, User, IBook, IUser} from './models';
-import {Library} from './library';
+import { Book, User, IBook, IUser } from "./models";
+import { Library } from "./library";
 
 export class LibraryService {
-    
+    static borrowBook(book: Book, user: User): boolean {
+        if (user.borrowedBooks.length >= 3) {
+            alert("Не можна позичити більше, ніж три книги.");
+            return false;
+        }
+        if (book.isBorrowed) {
+            alert("Книгу вже позичили.");
+            return false;
+        }
+        book.isBorrowed = true;
+        user.borrowBook(book.id);
+        return true;
+    }
+
+    static returnBook(book: Book, user: User): boolean {
+        if (!book.isBorrowed) {
+            alert("Книгу не позичали.");
+            return false;
+        }
+        if (!user.borrowedBooks.includes(book.id)) {
+            alert("Користувач не позичав цю книгу.")
+            return false
+        }
+
+        book.isBorrowed = false;
+        user.returnBook(book.id);
+        return true;
+    }
 }
