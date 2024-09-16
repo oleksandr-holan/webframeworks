c99e80bc96cb4476c1af79c21d0f793533e556ca
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-09-16 19:49:24 +0300

    implemented library class

--- lab3/src/library.ts
+++ lab3/src/library.ts
@@ -1,3 +1,19 @@
 export class Library<T> {
-    
+  private _items: T[] = [];
+
+  public get items(): T[] {
+    return this._items;
+  }
+
+  add(item: T): void {
+    this.items.push(item);
+  }
+
+  remove(item: T): void {
+    this._items = this.items.filter((i) => i !== item);
+  }
+
+  find(predicate: (item: T) => boolean): T | undefined {
+    return this._items.find(predicate);
+  }
 }
