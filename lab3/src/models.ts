c7e3765e21746b915049d77bfd9c6a8732268399
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-09-16 19:48:59 +0300

    implemented user class

--- lab3/src/models.ts
+++ lab3/src/models.ts
@@ -17,9 +17,25 @@ export class Book implements IBook {
 }
 
 export interface IUser {
-
+    id: number;
+    name: string;
+    email: string;
+    borrowedBooks: number[];
 }
 
 export class User implements IUser {
+    constructor(
+        public id: number,
+        public name: string,
+        public email: string,
+        public borrowedBooks: number[] = []
+    ) {}
 
+    borrowBook(book_id: number): void {
+        this.borrowedBooks.push(book_id);
+    }
+
+    returnBook(book_id: number): void {
+        this.borrowedBooks = this.borrowedBooks.filter((id) => id !== book_id);
+    }
 }
