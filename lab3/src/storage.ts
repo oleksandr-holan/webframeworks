031331a2ee01b9193e1b35b438b43a730ab5212f
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-09-16 19:50:19 +0300

    implemented Storage class

--- lab3/src/storage.ts
+++ lab3/src/storage.ts
@@ -1,3 +1,18 @@
 export class Storage {
+    static save(key: string, data: any): void {
+        localStorage.setItem(key, JSON.stringify(data));
+    }
 
+    static load(key: string): any {
+        const data = localStorage.getItem(key);
+        return data ? JSON.parse(data) : null;
+    }
+
+    static remove(key: string): void {
+        localStorage.removeItem(key);
+    }
+
+    static clear(): void {
+        localStorage.clear();
+    }
 }
