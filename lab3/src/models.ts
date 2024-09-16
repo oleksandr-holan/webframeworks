1da21bdd7d3a3cb6d373f401556caf1c48c470fd
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-09-16 19:48:26 +0300

    implemented book class

--- lab3/src/models.ts
+++ lab3/src/models.ts
@@ -1,9 +1,19 @@
 export interface IBook {
-
+    id: number;
+    title: string;
+    author: string;
+    year: number;
+    isBorrowed: boolean;
 }
 
 export class Book implements IBook {
-
+    constructor(
+        public id: number,
+        public title: string,
+        public author: string,
+        public year: number,
+        public isBorrowed: boolean = false
+    ) {}
 }
 
 export interface IUser {
