df636667c73821ea62c6aa2aead72e78059eb9c9
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-10-06 00:44:19 +0300

    library: use private attribute, addAll method

--- lab3/src/library.ts
+++ lab3/src/library.ts
@@ -5,16 +5,20 @@ export class Library<T> {
     return this._items;
   }
 
-  constructor(items: T[]) {
+  constructor(items: T[] = []) {
     this._items = items
   }
 
   add(item: T): void {
-    this.items.push(item);
+    this._items.push(item);
+  }
+
+  addAll(items: T[]): void {
+    this._items.push(...items);
   }
 
   remove(item: T): void {
-    this._items = this.items.filter((i) => i !== item);
+    this._items = this._items.filter((i) => i !== item);
   }
 
   find(predicate: (item: T) => boolean): T | undefined {
