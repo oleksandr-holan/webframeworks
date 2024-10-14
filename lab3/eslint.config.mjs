6418aa9413334f5680cecdd9f108a2bd30fed4e4
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-10-14 22:36:40 +0300

    integrate eslint with prettier

--- lab3/eslint.config.mjs
+++ lab3/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -13,8 +14,8 @@ export default [
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-        allowDefaultProject: true,
       },
     },
   },
+  eslintConfigPrettier,
 ];
