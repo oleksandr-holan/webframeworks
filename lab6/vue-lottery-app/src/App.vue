10b878ee7b95aa32981246dffd51d083d855ca5f
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-10-20 18:41:12 +0300

    removed extra styles, imported UsersTable in the App

--- lab6/vue-lottery-app/src/App.vue
+++ lab6/vue-lottery-app/src/App.vue
@@ -1,38 +1,20 @@
 <script setup lang="ts">
 import RegisterForm from './components/RegisterForm.vue'
 import WinnersBlock from './components/WinnersBlock.vue'
+import UsersTable from './components/UsersTable.vue'
 </script>
 
 <template>
-  <header><WinnersBlock></WinnersBlock></header>
-  <main><RegisterForm></RegisterForm></main>
+  <div class="container mt-5 custom-container">
+    <WinnersBlock />
+    <RegisterForm />
+    <UsersTable />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.custom-container {
+  max-width: 800px;
+  margin: 0 auto;
 }
 </style>
