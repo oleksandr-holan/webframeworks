9ca7f96c24af5fb5b9bea4f9f31653630f3acb3d
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-10-20 18:44:05 +0300

    set up testing-library

--- lab6/vue-lottery-app/eslint.config.js
+++ lab6/vue-lottery-app/eslint.config.js
@@ -3,6 +3,8 @@ import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import pluginPlaywright from 'eslint-plugin-playwright'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginJestDom from 'eslint-plugin-jest-dom'
+import pluginTestingLibrary from 'eslint-plugin-testing-library'
 
 export default [
   {
@@ -17,15 +19,25 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
+
+  {
+    files: ['src/**/__tests__/*'],
+    ...pluginTestingLibrary.configs['flat/vue'],
+  },
+
+  {
+    files: ['src/**/__tests__/*'],
+    ...pluginJestDom.configs['flat/recommended'],
   },
   skipFormatting,
 ]
