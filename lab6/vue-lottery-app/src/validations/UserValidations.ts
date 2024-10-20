e9d5d68752e36f0a09532a66ec48c4b9ae2a04ac
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-10-20 18:38:58 +0300

    implemented User class with validation

---
+++ lab6/vue-lottery-app/src/validations/UserValidations.ts
@@ -0,0 +1,19 @@
+export function isValidDate(date: string): boolean {
+  return !isNaN(Date.parse(date))
+}
+
+export function isValidEmail(email: string): boolean {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return regex.test(email)
+}
+
+export function isValidPhone(phone: string): boolean {
+  const regex = /^\+?[\d\s-]{10,}$/
+  return regex.test(phone)
+}
+
+export function isDateInPast(date: string): boolean {
+  const inputDate = new Date(date)
+  const currentDate = new Date()
+  return inputDate <= currentDate
+}
